8c3c1f41b4e828ca97906b72db20d5f056a01a95
Balázs Antal <balazs.antal@pixelephant.hu>
2011-12-19 21:54:16 +0100

    rails_admin column rename

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 	has_many :children, :class_name => "Category"
 	belongs_to :parent, :class_name => "Category",
-		:foreign_key => "parent_id"
+		:foreign_key => "category_id"
 	has_many :products
 end
