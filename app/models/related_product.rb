3695a657004576a08ed31094fdcc843ec13a539c
Balázs Antal <balazs.antal@pixelephant.hu>
2012-01-09 10:54:44 +0100

    related_product -> validation => required
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- app/models/related_product.rb
+++ app/models/related_product.rb
@@ -1,4 +1,9 @@
 class RelatedProduct < ActiveRecord::Base
 	belongs_to :product
 	belongs_to :related_product, :class_name => "Product", :foreign_key => "related_product_id"
+
+	validates :related_product_id, :product_id, :presence => true
+
+	validates :related_product_id, :uniqueness => { :scope => :product_id,
+    :message => "Existing relation" }
 end
