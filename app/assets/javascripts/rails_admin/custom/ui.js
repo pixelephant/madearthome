5c4481064b833192b0d4a5a6ab4e42d3f1e9bbe8
Balázs Antal <balazs.antal@pixelephant.hu>
2012-01-16 16:38:43 +0100

    admin -> dynamic category properties
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -2,17 +2,25 @@ $("document").ready(function(){
 
 		$(".ui-menu-item").live('click', function(){
 			var category_id = $("#product_category_id_field option").val();
-//			alert(category_id);
+
 			$.post("/admin_ajax/getcategoryproperties",{
     		category_id : category_id
     	},function(resp){
 				if(resp.error == "none"){
+//					$("#product_property_ids_field  .ra-multiselect-left  .ra-multiselect-collection").html("");
+					var html = "";
 					$.each(resp.prop_ids, function(i, item){
 						console.log(item);
+						console.log(resp.prop_names[i]);
+						html += createOption(item, resp.prop_names[i]);
 					});
+					$("#product_property_ids_field  .ra-multiselect-left  .ra-multiselect-collection").html(html);
 				}
-
     	}, "json");
 		});
 
 });
+
+function createOption(value, text){
+	return '<option value="' + value + '">' + text + '</option>';
+}
--- app/controllers/admin_ajax_controller.rb
+++ app/controllers/admin_ajax_controller.rb
@@ -1,29 +1,27 @@
 class AdminAjaxController < ApplicationController
 	def getCategoryProperties
 		respond_to do |format|
+
 			cat = params[:category_id].to_i
-			msg = "["
+			property_ids = []
+			property_names = []
 
 			if Category.exists?(cat)
-#				msg = Category.find(cat).property_categories.any? ? Category.find(cat).property_categories.to_json(:only => [:id]) : ""
 				Category.find(cat).property_categories.each do |category|
-					msg << category.properties.to_json(:only => [:id]) if category.properties.any?
+					category.properties.each do |property|
+						property_ids << property.id
+						property_names << property.property_name
+					end
 				end
 			end
 
-#			msg2 = Property.where("property_category_id IS NULL").any? ? Property.where("property_category_id IS NULL").to_json(:only => [:id]) : ""
-
-				#Property.where("property_category_id IS NULL").each do |property|
-					#msg << property.to_json(:only => [:id])
-				#end
-
-			msg << Property.where("property_category_id IS NULL").to_json(:only => [:id]) if Property.where("property_category_id IS NULL").any?
-
-			msg << "]"
-
+			Property.where("property_category_id IS NULL").each do |property|
+				property_ids << property.id
+				property_names << property.property_name
+			end
 
 			format.json {
-				render :json => {:error => "none", :prop_ids => msg}
+				render :json => {:error => "none", :prop_ids => property_ids.as_json, :prop_names => property_names.as_json}
 			}
 		end
 	end
