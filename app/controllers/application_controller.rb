01eb155425768a6ddecdcb88c63d3dd062d34039
Balázs Antal <balazs.antal@pixelephant.hu>
2012-03-12 12:35:18 +0100

    1
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 	before_filter :set_i18n_locale_from_params
-  
+
   layout "application"
 
 	private
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
-    admin_path
+		if current_user.try(:admin?)
+			admin_path
+		else
+			"/"
+		end
   end
+
+	def after_sign_in_path_for(resource_or_scope)
+		if current_user.try(:admin?)
+			admin_path
+		else
+			"/"
+		end
+  end
+
 
 	def current_cart
 		Cart.find(session[:cart_id])
@@ -18,7 +31,7 @@ class ApplicationController < ActionController::Base
 		session[:cart_id] = cart.id
 		cart
 	end
-	
+
 	#I18n
 	def set_i18n_locale_from_params
 		if params[:locale]
