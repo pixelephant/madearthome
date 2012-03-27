d0f8ec7fd272d35cca80374c8f31d725e19cf6e0
Balázs Antal <balazs.antal@pixelephant.hu>
2012-03-27 14:17:54 +0200

    Login/register
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ActiveRecord::Base
 	belongs_to :user
+
+	attr_accessible :zip, :city, :additional
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :address, :addresses
+  	attr_accessible :email, :password, :password_confirmation, :remember_me, :addresses_attributes
 	has_many :addresses
 	has_many :orders
 	has_one :wishlist
