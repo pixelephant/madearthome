b97bc4dc6653f02dc6c208261f2c83fc9f134799
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-06-08 13:30:19 +0200

    blog integráció
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   layout "home"
   
   def index
-
+  	@blogposts = WpBlogPost.limit(2).order('post_date desc').where(:post_status => 'publish')
   end
 end
