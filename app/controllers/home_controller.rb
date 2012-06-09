63b07eed24175aa50f052e60fabfa3bb37c932e3
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-06-09 13:01:22 +0200

    apróságok
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,7 @@ class HomeController < ApplicationController
   
   def index
   	@blogposts = WpBlogPost.limit(2).order('post_date desc').where(:post_status => 'publish')
+  	# @blogposts = []
+  	@randomProduct = Product.order("RAND()").limit(1).first
   end
 end
