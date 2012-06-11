238dbb6a895c47c22026f04856d2c20fa7d38023
Balázs Antal <balazs.antal@pixelephant.hu>
2012-06-11 13:34:31 +0200

    random search term keresés route
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- config/initializers/app_config_init.rb
+++ config/initializers/app_config_init.rb
@@ -2,3 +2,5 @@
 
 DESCRIPTION = "Flairspring - Bútor, lámpa, tükör, komfort fotel"
 KEYWORDS = "Flairspring keywords - Bútor, lámpa, tükör, komfort fotel"
+
+SEARCH_TERMS = "kör alakú tükrök,modern tükrök,órák,könyvtámasz,festmények,absztrakt faldekor,hintázó komfort fotelek,motoros komfort fotelek,bőrkanapé,vendégágyas kanapék,komódok,dohányzó asztalok,asztali lámpa,állólámpa,csillárok".split(",")
