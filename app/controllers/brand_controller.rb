6f5bedf54679dd4a81e496a05882c65c409f97e2
Pixelephant Media <pixelephant@pixelephant.hu>
2012-06-11 17:22:11 +0200

    no message

--- app/controllers/brand_controller.rb
+++ app/controllers/brand_controller.rb
@@ -8,6 +8,7 @@ class BrandController < ApplicationController
   end
 
   def uttermost
+  	@products = Product.where(:sku => ["06657","13364 p","14028 b","19431","19506","19550","24203","26460","26685","34230","60104"])
   	render "uttermost"
   end
 
